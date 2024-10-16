803dfc0b36097ddb86a253fe8295fc7b38b2a05d
trimeritube <jeanricardo.jr123@gmail.com>
2024-10-16 14:53:07 -0300

    indexador criado

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indexador de Atividades</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+        h1 {
+            color: #2c3e50;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            margin-bottom: 15px;
+        }
+        a {
+            text-decoration: none;
+            color: #2980b9;
+            font-size: 18px;
+        }
+        a:hover {
+            color: #e74c3c;
+        }
+    </style>
+</head>
+<body>
+
+    <h1>Indexador de Atividades</h1>
+    <p>Abaixo está a lista de atividades que você poderá acessar:</p>
+
+    <ul>
+        <li>
+            <a href="atividade1/index.php">Atividade 1: Saudação com Imagens</a>
+            <p>Exibe uma saudação de acordo com a hora do dia e mostra uma imagem correspondente.</p>
+        </li>
+        <li>
+            <a href="atividade2/index.php">Atividade 2: Formulário de Cadastro</a>
+            <p>Criação de um formulário de cadastro de usuários com validações.</p>
+        </li>
+        <li>
+            <a href="atividade3/index.php">Atividade 3: Calculadora Simples</a>
+            <p>Uma calculadora PHP que realiza operações básicas de adição, subtração, multiplicação e divisão.</p>
+        </li>
+        <li>
+            <a href="atividade4/index.php">Atividade 4: Upload de Arquivos</a>
+            <p>Criação de uma página que permite upload de arquivos e exibe uma lista de arquivos enviados.</p>
+        </li>
+        <!-- Adicione aqui outras atividades conforme necessário -->
+    </ul>
+
+</body>
+</html>
